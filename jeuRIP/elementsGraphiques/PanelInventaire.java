506e748efd4e20c5c964b9fd2eedd77bbfab5415
khamisMiage <47559500+khamisMiage@users.noreply.github.com>
2019-03-15 14:23:20 +0100

    Update PanelInventaire.java

--- jeuRIP/elementsGraphiques/PanelInventaire.java
+++ jeuRIP/elementsGraphiques/PanelInventaire.java
@@ -188,10 +188,23 @@ public class PanelInventaire extends JPanel {
 			
 		}
 	    
-	    
+	    /*
 	    public void setImageDeFondLbl (String nomFichier, JLabel lbl) {
 			imgLoader.setImageDeFondLbl(nomFichier, lbl);
 		}
+*/
+		
+	    
+	    public void setImageDeFondLbl (String nomFichier, JLabel lbl) {
+			//System.out.println(this.getClass().getResource("/images/"+ nomFichier)); // debug
+			lbl.setIcon(null);
+			ImageIcon icon = new ImageIcon( this.getClass().getResource("/images/"+ nomFichier));
+		    Image img = icon.getImage();
+		    Image newImg = img.getScaledInstance(lbl.getWidth(), lbl.getHeight(), Image.SCALE_SMOOTH);
+		    ImageIcon newIcon = new ImageIcon(newImg);
+		    lbl.setIcon(newIcon);
+		}
 
 		
+
 }
