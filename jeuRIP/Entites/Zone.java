e2ebd0d461f5171c147385d1c1ce3c09b67e7b48
jbmontecristo <47559441+jbmontecristo@users.noreply.github.com>
2019-03-18 16:03:06 +0100

    Update Zone.java

--- jeuRIP/Entites/Zone.java
+++ jeuRIP/Entites/Zone.java
@@ -36,8 +36,13 @@ public class Zone   {
 			this.nomImage = nomImage;
 			this.sorties = new HashMap<String, Zone>();
 			this.listItemZone= new HashMap< Integer,Item>();
+			this.PNJZone = null;
 		}
 		
+		public PersoNonJoueur getPNJZone() {
+			return PNJZone;
+		}
+
 		public void ajouteSortie(Sortie sortie, Zone zoneVoisine) {
 			sorties.put(sortie.name(), zoneVoisine);
 		}
